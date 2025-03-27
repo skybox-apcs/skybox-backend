1fcdc32f99b565b2b827b2f047d650b6de9eecc6
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2025-03-27 13:10:27 +0700

    KAN-1: Setup the init codebase.

---
+++ configs/configs.go
@@ -0,0 +1,55 @@
+package configs
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type AppConfig struct {
+	// Server Config
+	ServerPort  string
+	ServerHost  string
+	ReleaseMode bool
+
+	// Database Config
+	MongoURI string
+
+	// JWT Config
+	JWTSecret string
+
+	// AWS Config
+	AWSKey    string
+	AWSSecret string
+	AWSBucket string
+	AWSRegion string
+}
+
+var Config AppConfig = AppConfig{}
+
+func LoadConfig() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found or error loading .env file. Using default values.")
+	}
+
+	// Load the environment variables
+	Config.ServerPort = getEnv("SERVER_PORT", "8080")
+	Config.ServerHost = getEnv("SERVER_HOST", "localhost")
+	Config.ReleaseMode = getEnv("GIN_MODE", "debug") == "release"
+	Config.MongoURI = getEnv("MONGO_URI", "mongodb://localhost:27017")
+	Config.JWTSecret = getEnv("JWT_SECRET_KEY", "secret")
+	Config.AWSKey = getEnv("AWS_SECRET_KEY_ID", "")
+	Config.AWSSecret = getEnv("AWS_SECRET_ACCESS_KEY", "")
+	Config.AWSBucket = getEnv("AWS_BUCKET_NAME", "")
+	Config.AWSRegion = getEnv("AWS_REGION", "us-east-1")
+}
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return fallback
+}
