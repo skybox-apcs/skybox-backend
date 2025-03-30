b76cbff9c8728bacb9091b752b118f68134be887
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2025-03-30 15:57:13 +0700

    Fix min max password

--- internal/models/auth_domain.go
+++ internal/models/auth_domain.go
@@ -2,8 +2,8 @@ package models
 
 type RegisterRequest struct {
 	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required, min=8"`
-	Username string `json:"username" binding:"required, max=32"`
+	Password string `json:"password" binding:"required,min=8"`
+	Username string `json:"username" binding:"required,max=32"`
 }
 
 type LoginRequest struct {
