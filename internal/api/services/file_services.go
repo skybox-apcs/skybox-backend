22d94462af5079fdc679299de3b6ad0a26c1e0a4
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2025-04-29 13:40:11 +0700

    SAD-14: Modify file services for uploading.

--- internal/api/services/file_services.go
+++ internal/api/services/file_services.go
@@ -2,7 +2,9 @@ package services
 
 import (
 	"context"
+	"fmt"
 
+	"skybox-backend/configs"
 	"skybox-backend/internal/api/models"
 )
 
@@ -18,11 +20,36 @@ func NewFileService(fr models.FileRepository) *FileService {
 	}
 }
 
-func (fr *FileService) UploadFileMetadata(ctx context.Context, file *models.File) (*models.File, error) {
+// UploadFileMetadata uploads the metadata of a file and returns the saved file and upload URL
+// It also decides whether to create an upload session for chunked uploads or a single upload URL for whole file uploads
+// TODO: Handle concurrency and chunked uploads
+func (fr *FileService) UploadFileMetadata(ctx context.Context, file *models.File) (*models.File, string, error) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	return fr.fileRepository.UploadFileMetadata(ctx, file)
+	savedFile, err := fr.fileRepository.UploadFileMetadata(ctx, file)
+	if err != nil {
+		return nil, "", err
+	}
+
+	// Decided if chunked or whole file upload
+	const maxFileSize = 50 * 1024 * 1024 // 50MB. TODO: Move to config
+	var uploadURL string = ""
+
+	// If the file is large, create upload session and chunk records
+	if file.Size > maxFileSize {
+		// Create upload session and chunk records
+
+	} else {
+		// Create a single upload URL for the whole file
+		uploadURL = fmt.Sprintf("%s:%s/upload/whole/%s",
+			configs.Config.BlockServerHost,
+			configs.Config.BlockServerPort,
+			savedFile.ID.Hex(),
+		)
+	}
+
+	return savedFile, uploadURL, nil
 }
 
 func (fr *FileService) GetFileByID(ctx context.Context, id string) (*models.File, error) {
