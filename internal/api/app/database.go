065d996e6b23d20b4bebaadceff3d59b66130285
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2025-04-29 18:57:28 +0700

    SAD-14: Add indexes to database for more efficient query.

--- internal/api/app/database.go
+++ internal/api/app/database.go
@@ -3,9 +3,11 @@ package app
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"skybox-backend/configs"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -37,4 +39,66 @@ func CloseMongoDatabase(client *mongo.Client) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func CreateIndexes(db *mongo.Database) error {
+	// Create a context with a timeout for the index creation
+	// This is important to avoid blocking the application indefinitely
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Define a string - index mapping for the indexes to be created
+	indexes := map[string][]mongo.IndexModel{}
+
+	// Define the indexes for the "users" collection
+	indexes["users"] = []mongo.IndexModel{
+		{
+			Keys:    bson.D{{Key: "email", Value: 1}}, // Unique index on email
+			Options: options.Index().SetUnique(true),
+		},
+	}
+
+	// Define the indexes for the "files" collection
+	indexes["files"] = []mongo.IndexModel{
+		{
+			Keys: bson.D{
+				{Key: "owner_id", Value: 1},         // Index on owner_id
+				{Key: "parent_folder_id", Value: 1}, // Index on parent_folder_id
+				{Key: "name", Value: -1},            // Sort by name descending
+			}, // Sort by created_at descending
+		},
+	}
+
+	// Define the indexes for the "folders" collection
+	indexes["folders"] = []mongo.IndexModel{
+		{
+			Keys: bson.D{
+				{Key: "owner_id", Value: 1},         // Index on owner_id
+				{Key: "parent_folder_id", Value: 1}, // Index on parent_folder_id
+				{Key: "name", Value: -1},            // Sort by name descending
+			}, // Sort by created_at descending
+		},
+	}
+
+	// Define the indexes for the "user_tokens" collection
+	indexes["user_tokens"] = []mongo.IndexModel{
+		{
+			Keys: bson.D{
+				{Key: "user_id", Value: 1}, // Index on user_id
+				{Key: "token", Value: 1},   // Index on token
+			},
+		},
+	}
+
+	// Create the indexes for each collection using goroutines
+	for collectionName, indexModels := range indexes {
+		collection := db.Collection(collectionName)
+		_, err := collection.Indexes().CreateMany(ctx, indexModels)
+		if err != nil {
+			return fmt.Errorf("failed to create indexes for collection %s: %v", collectionName, err)
+		}
+		fmt.Printf("Indexes created for collection %s\n", collectionName)
+	}
+
+	return nil
 }
