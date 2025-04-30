35eee5a3792bcf651d811f75602fa55fddaf5ae9
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2025-04-30 10:37:42 +0700

    SAD-14: Refactor upload services to include chunk hashing and update session record structure

--- internal/blockserver/models/upload_domain.go
+++ internal/blockserver/models/upload_domain.go
@@ -6,3 +6,9 @@ type NonResumableUploadResponse struct {
 	FileSize   int64  `json:"file_size"`   // The size of the uploaded file in bytes
 	ChunkCount int    `json:"chunk_count"` // The number of chunks the file was split into (if applicable)
 }
+
+type AddChunkSessionRequest struct {
+	ChunkNumber int    `json:"chunk_number"`
+	ChunkSize   int    `json:"chunk_size"`
+	ChunkHash   string `json:"chunk_hash"`
+}
