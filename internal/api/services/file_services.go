fb9c6d24c8241052748373ca388db273c70e1916
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2025-04-30 09:53:47 +0700

    SAD-14: Refactor UploadFileMetadata to streamline chunked upload session creation

--- internal/api/services/file_services.go
+++ internal/api/services/file_services.go
@@ -37,52 +37,31 @@ func (fr *FileService) UploadFileMetadata(ctx context.Context, file *models.File
 		return nil, "", err
 	}
 
-	// Decided if chunked or whole file upload
-	var maxFileSize = configs.Config.MaxChunkSize
-	var uploadURL string = ""
-
-	// If the file is large, create upload session and chunk records
-	if true || file.Size > maxFileSize {
-		// Create upload session and chunk records
-		// uploadURL = fmt.Sprintf("%s:%s/upload/chunked/%s",
-		// 	configs.Config.BlockServerHost,
-		// 	configs.Config.BlockServerPort,
-		// 	savedFile.ID.Hex(),
-		// )
-
-		// Create a session for chunked uploads
-		totalChunks := int(math.Ceil(float64(file.Size) / float64(configs.Config.DefaultChunkSize)))
-		sessionToken := uuid.New().String()
-		uploadSession := &models.UploadSession{
-			FileID:       savedFile.ID,
-			UserID:       savedFile.OwnerID,
-			SessionToken: sessionToken,
-			TotalSize:    file.Size,
-			ChunkSize:    configs.Config.DefaultChunkSize,
-			ChunkList:    []int{}, // This will be updated later when chunks are uploaded
-			TotalChunks:  totalChunks,
-			Status:       "pending",
-		}
-
-		_, err = fr.uploadSessionRepository.CreateSessionRecord(ctx, uploadSession)
-		if err != nil {
-			return nil, "", fmt.Errorf("failed to create upload session: %w", err)
-		}
-
-		// Create the uploadURL based on the uploadSession ID
-		uploadURL = fmt.Sprintf("%s:%s/upload/session/%s/chunk",
-			configs.Config.BlockServerHost,
-			configs.Config.BlockServerPort,
-			sessionToken,
-		)
-	} else {
-		// Create a single upload URL for the whole file
-		uploadURL = fmt.Sprintf("%s:%s/upload/whole/%s",
-			configs.Config.BlockServerHost,
-			configs.Config.BlockServerPort,
-			savedFile.ID.Hex(),
-		)
+	// Create a session for chunked uploads
+	totalChunks := int(math.Ceil(float64(file.Size) / float64(configs.Config.DefaultChunkSize)))
+	sessionToken := uuid.New().String()
+	uploadSession := &models.UploadSession{
+		FileID:       savedFile.ID,
+		UserID:       savedFile.OwnerID,
+		SessionToken: sessionToken,
+		TotalSize:    file.Size,
+		ChunkSize:    configs.Config.DefaultChunkSize,
+		ChunkList:    []int{}, // This will be updated later when chunks are uploaded
+		TotalChunks:  totalChunks,
+		Status:       "pending",
 	}
+
+	_, err = fr.uploadSessionRepository.CreateSessionRecord(ctx, uploadSession)
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to create upload session: %w", err)
+	}
+
+	// Create the uploadURL based on the uploadSession ID
+	uploadURL := fmt.Sprintf("%s:%s/upload/session/%s/chunk",
+		configs.Config.BlockServerHost,
+		configs.Config.BlockServerPort,
+		sessionToken,
+	)
 
 	return savedFile, uploadURL, nil
 }
